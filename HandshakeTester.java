faaa6bf4d9c8c62d3bef5cdff4351448a8533f41
Shrinish Donde <shdonde@gmail.com>
2018-12-14 23:28:27 +0100

    Add files via upload

---
+++ HandshakeTester.java
@@ -0,0 +1,30 @@
+import java.security.*;
+
+
+public class HandshakeTester {
+	static String PRIVATEKEYFILE = "rsaprivatekey.der";
+	static String CERTFILE = "User.pem";
+	static String PLAINTEXT = "Time flies like an arrow. Fruit flies like a banana.";
+	static String ENCODING = "UTF-8"; /* For converting between strings and byte arrays */
+
+	static public void main(String[] args) throws Exception {
+
+		/* Extract key pair */
+		PublicKey publickey = HandshakeCrypto.getPublicKeyFromCertFile(CERTFILE);
+		PrivateKey privatekey = HandshakeCrypto.getPrivateKeyFromKeyFile(PRIVATEKEYFILE);
+
+		/* Encode string as bytes */
+		byte[] plaininputbytes = PLAINTEXT.getBytes(ENCODING);
+		/* Encrypt it */
+		byte[] cipher = HandshakeCrypto.encrypt(plaininputbytes, publickey);
+		/* Then decrypt back */
+		byte[] plainoutputbytes = HandshakeCrypto.decrypt(cipher, privatekey);
+		/* Decode bytes into string */
+		String plainoutput = new String(plainoutputbytes, ENCODING);
+		if (plainoutput.equals(PLAINTEXT)) {
+			System.out.println("Pass. Input and output strings are the same: \"" + PLAINTEXT + "\"");
+		} else {
+			System.out.println("Fail. Expected \"" + PLAINTEXT + "\", but got \"" + plainoutput + "\'");
+		}
+	}
+}
